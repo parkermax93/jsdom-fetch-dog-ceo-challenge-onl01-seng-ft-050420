92e5ec4fce8db25620ac1acaf9751cdf665a6a55
Parker Williamson <parker.williamson@gmail.com>
2020-08-10 16:58:50 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 console.log('%c HI', 'color: firebrick')
 
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+function fetchImg() { 
+  return fetch(imgUrl)
+  .then(resp => resp.json())
+  .then(json => renderImages(json.message));
+}
+
+function renderImages(images) {
+  const div = document.getElementByID('dog-image-contrainer');
+  images.forEach(imgSource => { 
+    const img = document.createElement('img');
+    img.src = imgSource;
+    div.appendChild(img);
+  });
+}
