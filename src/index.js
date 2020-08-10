7303a735a70caa02619d1e40dcbc1e1d7afeaa15
Parker Williamson <parker.williamson@gmail.com>
2020-08-10 17:07:30 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@ console.log('%c HI', 'color: firebrick')
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 
-function fetchImg() { 
+function fetchImg() {
   return fetch(imgUrl)
   .then(resp => resp.json())
   .then(json => renderImages(json.message));
-}
+};
 
 function renderImages(images) {
-  const div = document.getElementByID('dog-image-contrainer');
-  images.forEach(imgSource => { 
+  const div = document.getElementById('dog-image-container');
+  images.forEach(imgSource => {
     const img = document.createElement('img');
     img.src = imgSource;
     div.appendChild(img);
   });
-}
+};
 
 function fetchBreed() {
   return fetch(breedUrl)
@@ -25,32 +25,32 @@ function fetchBreed() {
 }
 
 function renderBreed(breeds) {
-  const ul = document.getElementByID('dog-breeds');
+  const ul = document.getElementById('dog-breeds');
   const select = document.getElementById('breed-dropdown');
-  
+
   breeds.forEach(breed => {
     const li = document.createElement('li');
     li.innerText = breed;
     ul.appendChild(li);
-    
+
     li.addEventListener('click', function() {
       li.style.color = 'firebrick';
     })
   })
-  
-  select.addeventListener('change', function() {
+
+  select.addEventListener('change', function() {
     const letter = select.value;
     const lis = ul.getElementsByTagName('li');
-    Array.from(list).forEach(li => {
+    Array.from(lis).forEach(li => {
       const firstLetter = li.innerText[0];
       if (firstLetter == letter) {
         li.style.display = '';
-      } else { 
+      } else {
         li.style.display = 'none';
       }
     })
   });
-}
+};
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchImg();
